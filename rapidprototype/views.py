3875306173be50938f80c0dbeb327741304aaf42
Ace <ace.github@gmail.com>
2014-12-16 17:03:05 -0800

    Added metadata to pages

--- rapidprototype/views.py
+++ rapidprototype/views.py
@@ -1,9 +1,11 @@
 import os
+import json
 
 from django.conf import settings
 from django.http import Http404
 from django.shortcuts import render
-from django.template import Template
+from django.template import Template, Context
+from django.template.loader_tags import BlockNode
 from django.utils._os import safe_join
 
 def get_page_or_404(name):
@@ -19,6 +21,14 @@ def get_page_or_404(name):
 	with open(file_path, 'r') as f:
 		page = Template(f.read())
 
+	meta = None
+	for i, node in enumerate(list(page.nodelist)):
+		if isinstance(node, BlockNode) and node.name == 'context':
+			meta = page.nodelist.pop(i)
+			break
+
+	page._meta = meta
+
 	return page
 
 def page(request, slug='index'):
@@ -26,4 +36,8 @@ def page(request, slug='index'):
 	file_name = '{}.html'.format(slug)
 	page = get_page_or_404(file_name)
 	context = {'slug':slug, 'page':page,}
+	if page._meta is not None:
+		meta = page._meta.render(Context())
+		extra_content = json.loads(meta)
+		context.update(extra_content)
 	return render(request, 'page.html', context)
