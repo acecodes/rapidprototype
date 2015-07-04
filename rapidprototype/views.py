69bac7c70550c97598146b323f9224cc30cdcd30
Ace <ace.github@gmail.com>
2015-07-04 16:08:14 -0700

    PEP 8 formatting

--- rapidprototype/views.py
+++ rapidprototype/views.py
@@ -8,36 +8,38 @@ from django.template import Template, Context
 from django.template.loader_tags import BlockNode
 from django.utils._os import safe_join
 
+
 def get_page_or_404(name):
-	"""Return Django template or 404"""
-	try:
-		file_path = safe_join(settings.SITE_PAGES_DIRECTORY, name)
-	except ValueError:
-		raise Http404('Page Not Found')
-	else:
-		if not os.path.exists(file_path):
-			raise Http404('Page Not Found')
+    """Return Django template or 404"""
+    try:
+        file_path = safe_join(settings.SITE_PAGES_DIRECTORY, name)
+    except ValueError:
+        raise Http404('Page Not Found')
+    else:
+        if not os.path.exists(file_path):
+            raise Http404('Page Not Found')
 
-	with open(file_path, 'r') as f:
-		page = Template(f.read())
+    with open(file_path, 'r') as f:
+        page = Template(f.read())
 
-	meta = None
-	for i, node in enumerate(list(page.nodelist)):
-		if isinstance(node, BlockNode) and node.name == 'context':
-			meta = page.nodelist.pop(i)
-			break
+    meta = None
+    for i, node in enumerate(list(page.nodelist)):
+        if isinstance(node, BlockNode) and node.name == 'context':
+            meta = page.nodelist.pop(i)
+            break
 
-	page._meta = meta
+    page._meta = meta
 
-	return page
+    return page
+
 
 def page(request, slug='index'):
-	"""Render the requested page if found"""
-	file_name = '{}.html'.format(slug)
-	page = get_page_or_404(file_name)
-	context = {'slug':slug, 'page':page,}
-	if page._meta is not None:
-		meta = page._meta.render(Context())
-		extra_content = json.loads(meta)
-		context.update(extra_content)
-	return render(request, 'page.html', context)
+    """Render the requested page if found"""
+    file_name = '{}.html'.format(slug)
+    page = get_page_or_404(file_name)
+    context = {'slug': slug, 'page': page, }
+    if page._meta is not None:
+        meta = page._meta.render(Context())
+        extra_content = json.loads(meta)
+        context.update(extra_content)
+    return render(request, 'page.html', context)
